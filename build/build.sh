a105f1c371aadf7effe239859798af3b0c085cb4
Benoit Vianin <16624067+benvia@users.noreply.github.com>
2025-10-16 16:05:30 +0000

    Reduve size of module by removing a set of unsed files in dependencies

--- build/build.sh
+++ build/build.sh
@@ -8,6 +8,26 @@ if [ -d includes/vendor/sprain/swiss-qr-bill/example ]; then
   rm -rf includes/vendor/sprain/swiss-qr-bill/example
 fi
 
+if [ -d includes/vendor/symfony/intl/Resources/data/currencies ]; then
+  rm -rf includes/vendor/symfony/intl/Resources/data/currencies
+fi
+
+if [ -d includes/vendor/symfony/intl/Resources/data/languages ]; then
+  rm -rf includes/vendor/symfony/intl/Resources/data/languages
+fi
+
+if [ -d includes/vendor/symfony/intl/Resources/data/timezones ]; then
+  rm -rf includes/vendor/symfony/intl/Resources/data/timezones
+fi
+
+if [ -d includes/vendor/symfony/intl/Resources/data/transliterator ]; then
+  rm -rf includes/vendor/symfony/intl/Resources/data/transliterator
+fi
+
+if [ -d includes/vendor/endroid/qr-code/assets ]; then
+  rm -rf includes/vendor/endroid/qr-code/assets
+fi
+
 cd ..
 
 if [ -f swissqr.zip ]; then
