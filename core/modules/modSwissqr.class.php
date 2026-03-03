ac1721addcdb9fad69063890c69ae45656c7e581
Benoit Vianin <16624067+benvia@users.noreply.github.com>
2026-03-03 10:05:43 +0000

    Update version

--- core/modules/modSwissqr.class.php
+++ core/modules/modSwissqr.class.php
@@ -75,7 +75,7 @@ class modSwissqr extends DolibarrModules
 		$this->editor_url = 'https://sinabe.ch';
 
 		// Possible values for version are: 'development', 'experimental', 'dolibarr', 'dolibarr_deprecated', 'experimental_deprecated' or a version string like 'x.y.z'
-		$this->version = '2.0.0';
+		$this->version = '2.0.1';
 		// Url to the file with your last numberversion of this module
 		$this->url_last_version = 'https://modules-dolibarr.sinabe.ch/swissqr/versionmodule.txt';
 
